28f12f8ece59a31f66652e6565191e5be015b4d5
Richard Fenix <richard.fenix@gmail.com>
2018-04-09 14:35:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,14 @@
+def current_player(board)
+  player = (turn_count(board) % 2 == 0) ? "X" : "O"
+end    
+  
+
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn[counter] = "X" || turn[counter] == "O"
+    if turn == "X" || turn == "O"
       counter += 1
+    end
   end
+  return counter
 end
